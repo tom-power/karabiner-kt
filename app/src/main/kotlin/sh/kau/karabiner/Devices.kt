2ccc0266ab34ee52299ce95ea2d60cec575ba735
kaush <1042037+kaushikgopal@users.noreply.github.com>
2025-05-08 23:19:16 -0400

    remove unnecssary files

--- app/src/main/kotlin/sh/kau/karabiner/Devices.kt
+++
@@ -1,9 +0,0 @@
-package sh.kau.karabiner
-
-// Using the Identifiers data class from Types.kt
-// If DEVICE entries are directly serialized as part of DEVICE_CONFIGS,
-// then Identifiers and its fields should be @Serializable and use @SerialName if needed.
-// For now, assuming they are primarily for constructing DeviceIfCondition etc.
-
-// We'll define DEVICE_COMBO and DEVICE_CONFIGS next.
-// For DEVICE_CONFIGS, we'll need a new data class.
