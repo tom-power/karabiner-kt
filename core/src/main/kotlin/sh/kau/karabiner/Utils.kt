c170ad3780199c6cb7589fc201d8d3f2713d2448
Tom Power <tpower21@gmail.com>
2025-06-24 14:46:51 +0100

    jsonEncoder -> json

--- core/src/main/kotlin/sh/kau/karabiner/Utils.kt
+++ core/src/main/kotlin/sh/kau/karabiner/Utils.kt
@@ -4,12 +4,10 @@ package sh.kau.karabiner
 
 import kotlinx.serialization.json.Json
 
-fun jsonEncoder(complexModifications: ComplexModifications): String {
-    val jsonEncoder = Json {
+fun json(): Json {
+    return Json {
         prettyPrint = true
         encodeDefaults = true
         explicitNulls = false // Don't serialize null values
     }
-
-    return jsonEncoder.encodeToString<ComplexModifications>(complexModifications)
 }
